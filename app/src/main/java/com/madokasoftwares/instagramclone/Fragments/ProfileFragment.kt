1ae191813cd8eb04d8112b835c05f58747e26bd1
marsdenmadoka <marsmadoka98@gmail.com>
2020-10-26 13:55:39 +0300

    added new files

--- app/src/main/java/com/madokasoftwares/instagramclone/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/madokasoftwares/instagramclone/Fragments/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.madokasoftwares.instagramclone.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.madokasoftwares.instagramclone.AccountSettingActivity
 import com.madokasoftwares.instagramclone.R
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,10 +35,16 @@ class ProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
+
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+     val view =inflater.inflate(R.layout.fragment_profile, container, false)
+    view.edit_account_settings_btn.setOnClickListener {
+        startActivity(Intent(context,AccountSettingActivity::class.java))
+    }
+
+    return view
     }
 
     companion object {
