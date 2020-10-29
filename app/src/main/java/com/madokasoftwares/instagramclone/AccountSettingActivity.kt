c596bef275f57036f57b8e09a59b8395e024d37a
marsdenmadoka <marsmadoka98@gmail.com>
2020-10-29 18:28:01 +0300

    added new files

--- app/src/main/java/com/madokasoftwares/instagramclone/AccountSettingActivity.kt
+++ app/src/main/java/com/madokasoftwares/instagramclone/AccountSettingActivity.kt
@@ -3,6 +3,8 @@ package com.madokasoftwares.instagramclone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
@@ -16,11 +18,15 @@ import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class AccountSettingActivity : AppCompatActivity() {
     private lateinit var firebaseUser: FirebaseUser
+     private var checker =""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_setting)
-
         firebaseUser=FirebaseAuth.getInstance().currentUser!!
+
+
+        DisplayUserInfo() //to understand how the code work practical check the xml file
+
 
         logOut_btn.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -29,14 +35,44 @@ class AccountSettingActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-        DisplayUserInfo() //to understand how the code work practical check the xml file
 
+        save_info_profile_btn.setOnClickListener {
+
+            if(checker == "clicked") //if profile image
+            {
+
+            }else{ //if no profile image
+                updateUserInfoOnly()
+            }
+        }
+    }
+
+    private fun updateUserInfoOnly() //update to firebase
+    {
+        when{
+            TextUtils.isEmpty(full_name.toString())-> Toast.makeText(this,"full name is required", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(username.toString())-> Toast.makeText(this,"username is required", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(bio.toString())-> Toast.makeText(this,"About is required", Toast.LENGTH_LONG).show()
+            else ->{
+                val usersRef= FirebaseDatabase.getInstance().getReference().child("Users").child(firebaseUser.uid)
+
+                val userMap=HashMap<String,Any>()
+                userMap["fullname"]=full_name.text.toString().toLowerCase() //remember the names of the usermap e.g userMap["fullname"] shoiuld be the same as those of signUp activity to avoid overwritting
+                userMap["username"]=username.text.toString().toLowerCase()
+                userMap["bio"]=bio.text.toString().toLowerCase()
+
+                usersRef.child(firebaseUser.uid).updateChildren(userMap) //updateChildren since we are updating/changing the info in the database when is already there
+
+                Toast.makeText(this,"Account info update successfully",Toast.LENGTH_LONG).show()
+                val intent = Intent(this@AccountSettingActivity,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 
 
-
-
-    private fun DisplayUserInfo(){
+    private fun DisplayUserInfo(){ //display to account activity.xml
         val usersRef= FirebaseDatabase.getInstance().getReference().child("Users").child(firebaseUser.uid)
         usersRef.addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
